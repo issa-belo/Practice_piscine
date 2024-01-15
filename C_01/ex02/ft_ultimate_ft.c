189992087c37c8dc87ea090ea25d9ae77c284e72
issaali1 <essa64069@gmail.com>
2024-01-15 19:36:26 +0100

    ex02

---
+++ C_01/ex02/ft_ultimate_ft.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr=42;
+}
+
+int main()
+{
+	int ptr;
+	int *ptr1=&ptr;
+	int **ptr2=&ptr1;
+	int ***ptr3=&ptr2;
+	int ****ptr4=&ptr3;
+	int *****ptr5=&ptr4;
+	int ******ptr6=&ptr5;
+	int *******ptr7=&ptr6;
+	int ********ptr8=&ptr7;
+	int *********ptr9=&ptr8;
+
+	ft_ultimate_ft(ptr9);
+	printf("%d\n", *********ptr9);
+}
